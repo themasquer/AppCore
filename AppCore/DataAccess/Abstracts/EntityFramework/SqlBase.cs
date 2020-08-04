2fe0fabac2f3fcc11f9c1ecc2a2cf024b104d4b2
Çağıl Alsaç <cagilalsac@outlook.com>
2020-08-04 16:35:42 +0300

    v35

---
+++ AppCore/DataAccess/Abstracts/EntityFramework/SqlBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCore.DataAccess.Abstracts.EntityFramework
+{
+    public abstract class SqlBase
+    {
+        private readonly DbContext _context;
+
+        protected SqlBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual void ExecuteSql(string sql)
+        {
+            try
+            {
+                _context.Database.ExecuteSqlRaw(sql);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ AppCore/DataAccess/Concretes/EntityFramework/Sql.cs
@@ -0,0 +1,13 @@
+﻿using AppCore.DataAccess.Abstracts.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCore.DataAccess.Concretes.EntityFramework
+{
+    public class Sql : SqlBase
+    {
+        public Sql(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
