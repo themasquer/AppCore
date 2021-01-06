5d6de03de5de9a0234456223d6e423c60acf4272
Çağıl Alsaç <cagilalsac@outlook.com>
2021-01-07 02:38:38 +0300

    v38

--- AppCore/DataAccess/Abstracts/EntityFramework/RepositoryBase.cs
+++ AppCore/DataAccess/Abstracts/EntityFramework/RepositoryBase.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AppCore.Records.Abstracts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using AppCore.Records.Abstracts;
 
 namespace AppCore.DataAccess.Abstracts.EntityFramework
 {
